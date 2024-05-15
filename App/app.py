9736123a2ea29d3fd4a6bbfd0dde53690b9b0aee
Francois Salmon <francois.salmon@semifir.com>
2024-05-15 16:47:59 +0200

    ci: :construction_worker: mise à jour du port de flask

--- App/app.py
+++ App/app.py
@@ -10,7 +10,7 @@ app.register_blueprint(main_bp)
 
 '''Exécution de l'application si elle est directement exécutée'''
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 """
 Flask est importé depuis le module flask. 
